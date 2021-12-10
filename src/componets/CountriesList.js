f65eaf79e24eea67501b86ef167c0afd6dda76a7
konhin2 <maskmesta@gmail.com>
2021-12-10 16:33:06 -0600

    done

--- src/componets/CountriesList.js
+++ src/componets/CountriesList.js
@@ -1,9 +1,17 @@
 import { Link, Outlet } from 'react-router-dom';
+import { useState } from 'react';
 
 const CountriesList = (props) => {
   // Es un arreglo de objetos
   const { allCountries } = props;
   // El .map sirve para recorrer un arreglo y te duvuelve uno nuevo con el mismo largo que el orginal, tu puedes modificar los datos que quieres mostrar
+  // Mensaje del input guardado en un estado local
+  const [searchValue, setSerchValue] = useState('')
+  // Funcion para detectar cambios en el input
+  function handleChange(event){
+    event.preventDefault()
+    setSerchValue(event.target.value)
+  }
   return (
     <div className="container">
       <div className="row">
@@ -11,8 +19,15 @@ const CountriesList = (props) => {
           className="col-5"
           style={{ maxHeight: '90vh', overflow: 'scroll' }}
         >
+          <input onChange={(e) => handleChange(e)} className="form-control me-2" type="search" placeholder="Search" ariaLabel="Search" />
           <div className="list-group">
-            {allCountries.map((elemento, i) => {
+            {allCountries.filter(elementoF => {
+              if(searchValue === ''){
+                return elementoF
+              } else if(elementoF.name.common.toLowerCase().includes(searchValue.toLowerCase())){
+                return elementoF
+              } return null
+            }).map((elemento, i) => {
               return (
                 <Link
                   key={i}
